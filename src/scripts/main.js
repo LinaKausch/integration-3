330d8c9789e0ab8974879ca8c66ea74fade3578f
Lina Kausch <lina.kausch@student.howest.be>
2025-01-23 17:41:18 +0100

    hamburger menu

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,8 +1,12 @@
+document.documentElement.classList.add('js-enabled'); // Add a class to `<html>` for JavaScript-specific styles
+
 const hamburger = document.querySelector('.hamburger');
-const navMenu = document.querySelector('.nav-menu');
+const navMenu = document.querySelector('.nav__menu');
 
 const init = () => {
     hamburger.addEventListener('click', () => {
+        const isExpanded = hamburger.getAttribute('aria-expanded') === 'true';
+        hamburger.setAttribute('aria-expanded', !isExpanded); // Toggle ARIA state
         navMenu.classList.toggle('active');
     });
 }
